678ae817ecda910e7520e828955eab2b0c1a2130
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-11-04 23:16:30 -0700

    0217 solution

---
+++ 0217/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.lastIndexOf(nums[i]) !== i) {
+      return true;
+    }
+  }
+  return false;
+};
+console.log(containsDuplicate([1, 2, 3, 1])); // true
+console.log(containsDuplicate([1, 2, 3, 4])); // false
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
