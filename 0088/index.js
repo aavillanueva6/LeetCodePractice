ad17bc5901c1cede1b144e4708c77b2542ba4451
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-11-05 23:21:39 -0700

    0088 solution

---
+++ 0088/index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  for (let i = m, j = 0; i < nums1.length; i++, j++) {
+    nums1[i] = nums2[j];
+  }
+  // nums1.sort((a, b) => a - b);
+  return nums1.sort((a, b) => a - b);
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); //[1,2,2,3,5,6]
+console.log(merge([1], 1, [], 0)); //[1]
+console.log(merge([0], 0, [1], 1)); //[1]
