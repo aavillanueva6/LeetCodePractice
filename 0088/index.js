b487f0ccbf3a3b9b579917c95d77c17f7e0283f7
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-11-08 00:14:56 -0800

    removes commented line

--- 0088/index.js
+++ 0088/index.js
@@ -9,7 +9,6 @@ var merge = function (nums1, m, nums2, n) {
   for (let i = m, j = 0; i < nums1.length; i++, j++) {
     nums1[i] = nums2[j];
   }
-  // nums1.sort((a, b) => a - b);
   return nums1.sort((a, b) => a - b);
 };
 
