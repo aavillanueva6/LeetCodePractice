a3207a6ea50f6e83bff23cf648ff81400466bf45
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-11-18 15:40:12 -0700

    increases code efficiency, reduce runtime from 4243ms to 115ms

--- problems/0217/index.js
+++ problems/0217/index.js
@@ -3,12 +3,22 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
+  let seen = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (!seen.has(nums[i])) seen.add(nums[i]);
+    else return true;
+  }
+  return false;
+
+  //original solution below
+  /*
   for (let i = 0; i < nums.length; i++) {
     if (nums.lastIndexOf(nums[i]) !== i) {
       return true;
     }
   }
   return false;
+  */
 };
 console.log(containsDuplicate([1, 2, 3, 1])); // true
 console.log(containsDuplicate([1, 2, 3, 4])); // false
