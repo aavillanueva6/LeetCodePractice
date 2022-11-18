f6ee56aa7d46a32defb9ed5080a4f707394cd36c
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-11-18 14:29:57 -0700

    0709 solution

---
+++ problems/0709/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+  return s.toLowerCase();
+};
+
+console.log(toLowerCase('Hello')); //"hello"
+console.log(toLowerCase('here')); //"here"
+console.log(toLowerCase('LOVELY')); //"lovely"
