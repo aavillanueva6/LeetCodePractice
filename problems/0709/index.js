3b00f4ce45143ddf2f1d2c7de4da6ae46b8587e8
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-11-18 18:07:31 -0700

    updates to 0709

--- problems/0709/index.js
+++ problems/0709/index.js
@@ -3,7 +3,18 @@
  * @return {string}
  */
 var toLowerCase = function (s) {
-  return s.toLowerCase();
+  // return s.toLowerCase(); // using built in string method is trivial.  Algo below
+  let lowered = '';
+  let code;
+  for (let i = 0; i < s.length; i++) {
+    code = s.charCodeAt(i);
+    if (code < 97) {
+      lowered += String.fromCharCode(code + 32);
+    } else {
+      lowered += String.fromCharCode(code);
+    }
+  }
+  return lowered;
 };
 
 console.log(toLowerCase('Hello')); //"hello"
