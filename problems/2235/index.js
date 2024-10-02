ce8a0aea79343389195062739d3b78fd6e996dce
Alejandro Villanueva <aavillanueva6@gmail.com>
2024-10-02 10:12:40 -0700

    2235 solution

---
+++ problems/2235/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function (num1, num2) {
+  return num1 + num2;
+};
+
+// console.log(triangularSum([1, 2, 3, 4, 5])); //8
+
+// console.log(triangularSum([5])); //5
+
+console.log(sum(12, 5)); //17
+console.log(sum(-10, 4)); //-6
