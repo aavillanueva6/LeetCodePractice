4cb52c1e64b590ecffc153aae1b96def0c11c268
Alejandro Villanueva <aavillanueva6@gmail.com>
2024-10-03 09:41:26 -0700

    1342 solution

---
+++ problems/1342/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+  let steps = 0;
+  while (num > 0) {
+    if (num % 2 === 0) {
+      num = num / 2;
+    } else {
+      num -= 1;
+    }
+    steps++;
+  }
+  return steps;
+};
+
+console.log(numberOfSteps(14)); //6
+console.log(numberOfSteps(8)); //4
+console.log(numberOfSteps(123)); //12
